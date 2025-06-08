a77c119cfb0b197a76ec40bf15e59ca581abb094
Nikolaos Limnios <nlimnios@inttrust.gr>
2025-06-08 18:12:08 +0300

    update photo editor

--- src/components/PhotobookEditor.tsx
+++ src/components/PhotobookEditor.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useRef, useState } from "react";
+import { Canvas, FabricImage, Rect } from "fabric";
 
 export type LayoutOption =
   | "no-placeholders"
@@ -14,48 +15,13 @@ type UploadedImage = {
 
 export default function PhotobookEditor() {
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
-    const createRect = (
-      left: number,
-      top: number,
-      width: number,
-      height: number,
-    ) => {
-        createRect(
-          gap * 2 + (w - 3 * gap) / 2,
-          gap,
-          (w - 3 * gap) / 2,
-          h - 2 * gap,
-        );
-        createRect(
-          gap * 2 + (w - 3 * gap) / 2,
-          gap,
-          (w - 3 * gap) / 2,
-          (h - 3 * gap) / 2,
-        );
-        createRect(
-          gap,
-          gap * 2 + (h - 3 * gap) / 2,
-          w - 2 * gap,
-          (h - 3 * gap) / 2,
-        );
-        createRect(
-          gap * 2 + (w - 3 * gap) / 2,
-          gap,
-          (w - 3 * gap) / 2,
-          (h - 3 * gap) / 2,
-        );
-        createRect(
-          gap,
-          gap * 2 + (h - 3 * gap) / 2,
-          (w - 3 * gap) / 2,
-          (h - 3 * gap) / 2,
-        );
-        createRect(
-          gap * 2 + (w - 3 * gap) / 2,
-          gap * 2 + (h - 3 * gap) / 2,
-          (w - 3 * gap) / 2,
-          (h - 3 * gap) / 2,
-        );
+  const containerRef = useRef<HTMLDivElement | null>(null);
+  const [canvas, setCanvas] = useState<Canvas>();
+  const [layout, setLayout] = useState<LayoutOption>("one-placeholder");
+  const [images, setImages] = useState<UploadedImage[]>([]);
+  const [placeholders, setPlaceholders] = useState<Rect[]>([]);
+
+  useEffect(() => {
     if (canvasRef.current && !canvas) {
       const c = new Canvas(canvasRef.current, {
         width: 600,
@@ -131,78 +97,6 @@ export default function PhotobookEditor() {
     canvas.renderAll();
   }, [canvas, layout]);
 
-        canvas.requestRenderAll();
-      <div onDrop={onDrop} onDragOver={onDragOver}>
-    e.preventDefault();
-    const id = e.dataTransfer?.getData("text/plain");
-    const img = images.find((i) => i.id === id);
-    if (!img || !canvas || !canvasRef.current) return;
-
-    const canvasBounds = canvasRef.current.getBoundingClientRect();
-    const dropX = e.clientX - canvasBounds.left;
-    const dropY = e.clientY - canvasBounds.top;
-
-    const target = placeholders.find((ph) => {
-      const bounds = ph.getBoundingRect();
-      return (
-        <input
-          type="file"
-          accept="image/*"
-          onChange={(e) => e.target.files && handleAddImage(e.target.files[0])}
-        />
-        <select
-          value={layout}
-          onChange={(e) => setLayout(e.target.value as LayoutOption)}
-          className="border p-1"
-        >
-        dropY >= bounds.top &&
-        dropY <= bounds.top + bounds.height
-      );
-    });
-
-    if (!target) {
-      console.log("No placeholder under drop point");
-      return;
-    }
-
-    console.log("Loading image from:", img.url);
-
-    const isDataUrl = img.url.startsWith("data:");
-
-    console.log("isDataUrl: ", isDataUrl);
-
-    FabricImage.fromURL(
-      img.url,
-      isDataUrl ? {} : { crossOrigin: "anonymous" },
-      (image: FabricImage) => {
-        if (!image) {
-          console.error("Image could not be loaded");
-          return;
-        }
-
-        const { left, top, width, height } = target;
-
-        const scaleX = width / image.width!;
-        const scaleY = height / image.height!;
-        const scale = Math.min(scaleX, scaleY);
-
-        image.set({
-          left: left + width / 2,
-          top: top + height / 2,
-          originX: "center",
-          originY: "center",
-          scaleX: scale,
-          scaleY: scale,
-        });
-
-        image.setControlsVisibility({ mtr: true });
-        canvas.add(image);
-        canvas.setActiveObject(image);
-        canvas.renderAll();
-      }
-    );
-  };
-
   const handleAddImage = (file: File) => {
     const reader = new FileReader();
     reader.onload = () => {
@@ -216,6 +110,59 @@ export default function PhotobookEditor() {
   const onDragStart = (e: React.DragEvent<HTMLImageElement>, id: string) => {
     e.dataTransfer.setData("text/plain", id);
     console.log("Drag start:", id);
+  };
+
+  const handleDrop = (e: React.DragEvent<HTMLDivElement>) => {
+    e.preventDefault();
+    const id = e.dataTransfer?.getData("text/plain");
+    const img = images.find((i) => i.id === id);
+    if (!img || !canvas || !canvasRef.current) return;
+
+    const canvasBounds = canvasRef.current.getBoundingClientRect();
+    const dropX = e.clientX - canvasBounds.left;
+    const dropY = e.clientY - canvasBounds.top;
+
+    const target = placeholders.find((ph) => {
+      const bounds = ph.getBoundingRect();
+      return (
+        dropX >= bounds.left &&
+        dropX <= bounds.left + bounds.width &&
+        dropY >= bounds.top &&
+        dropY <= bounds.top + bounds.height
+      );
+    });
+
+    if (!target) {
+      console.log("No placeholder under drop point");
+      return;
+    }
+
+    FabricImage.fromURL(img.url, {}, (image: FabricImage) => {
+      if (!image) {
+        console.error("Image could not be loaded");
+        return;
+      }
+
+      const { left, top, width, height } = target;
+      const scaleX = width / image.width!;
+      const scaleY = height / image.height!;
+      const scale = Math.min(scaleX, scaleY);
+
+      image.set({
+        left: left + width / 2,
+        top: top + height / 2,
+        originX: "center",
+        originY: "center",
+        scaleX: scale,
+        scaleY: scale,
+      });
+
+      image.setControlsVisibility({ mtr: true });
+      canvas.add(image);
+
+      canvas.setActiveObject(image);
+      canvas.renderAll();
+    });
   };
 
   return (
